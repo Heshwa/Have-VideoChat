b8dc1291fe02f1bc6f9514d8306cb2b0fa8c5219
Heshwa <heshwabaskaran@gmail.com>
2020-07-06 17:08:54 +0530

    Displaying user's List in the MainActivity

--- app/src/main/java/com/heshwa/haveavideochat/MainActivity.java
+++ app/src/main/java/com/heshwa/haveavideochat/MainActivity.java
@@ -1,14 +1,124 @@
 package com.heshwa.haveavideochat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private RecyclerView mRecyclerViewUserList;
+    private UserAdapter mUserAdapter;
+    private ArrayList<String> userList;
+    private DatabaseReference userRef;
+    private ProgressDialog progressDialog;
+    public static ArrayList<String> userIds;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mRecyclerViewUserList = findViewById(R.id.recyculerViewUserList);
+        mRecyclerViewUserList.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        userList = new ArrayList<>();
+        mUserAdapter = new UserAdapter(MainActivity.this,userList);
+        mRecyclerViewUserList.setAdapter(mUserAdapter);
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        progressDialog= new ProgressDialog(MainActivity.this);
+        progressDialog.setTitle("Getting Users");
+        progressDialog.setMessage("Please wait until , getting all the users");
+        userIds = new ArrayList<>();
+        getUsersFromServer();
+
+
+
+
+
+
+    }
+
+    private void getUsersFromServer() {
+        progressDialog.show();
+        userRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if(snapshot.hasChild("Name"))
+                {
+                    if(!snapshot.getKey().equals(mAuth.getCurrentUser().getUid()))
+                    {
+                        userList.add(snapshot.child("Name").getValue().toString());
+                        userIds.add(snapshot.getKey());
+                        mUserAdapter.notifyDataSetChanged();
+                        progressDialog.dismiss();
+                    }
+
+                }
+
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.itemLogOut)
+        {
+            mAuth.signOut();
+            Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
---
+++ app/src/main/java/com/heshwa/haveavideochat/UserAdapter.java
@@ -0,0 +1,99 @@
+package com.heshwa.haveavideochat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private Context mContext;
+    private ArrayList<String> names;
+    private DatabaseReference userRef;
+    private FirebaseAuth mAuth;
+
+    public UserAdapter(Context context, ArrayList<String> names) {
+        mContext = context;
+        this.names = names;
+    }
+
+
+    @NonNull
+    @Override
+    public UserAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new UserAdapter.ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.user_item,parent,false)) ;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final UserAdapter.ViewHolder holder, final int position) {
+        holder.txtUserName.setText(names.get(position));
+        holder.imgVideoCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userRef = FirebaseDatabase.getInstance().getReference().child("Users");
+                mAuth = FirebaseAuth.getInstance();
+                HashMap<String,String> hashMap = new HashMap<>();
+                hashMap.put("calling",MainActivity.userIds.get(position));
+                userRef.child(mAuth.getCurrentUser().getUid()).child("Calling").child("calling")
+                        .setValue(MainActivity.userIds.get(position))
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful())
+                                {
+                                    userRef.child(MainActivity.userIds.get(position)).child("Ringing")
+                                            .child("ringing").setValue(mAuth.getCurrentUser().getUid())
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if(task.isSuccessful())
+                                                    {
+                                                        Intent intent = new Intent(mContext,CallingActivity.class);
+                                                        mContext.startActivity(intent);
+                                                    }
+                                                }
+                                            });
+                                }
+                            }
+                        });
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return names.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView txtUserName;
+        private ImageView imgVideoCall;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtUserName = itemView.findViewById(R.id.txtUserName);
+            imgVideoCall = itemView.findViewById(R.id.imgVideoCall);
+        }
+    }
+}
+
